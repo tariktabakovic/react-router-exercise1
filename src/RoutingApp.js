3ef4fe90ccfb42037882f6e8fa998142be7787bb
tariktabakovic <tariktab97@gmail.com>
2020-02-09 18:52:49 -0500

    installs react-router-dom, creates RoutingApp component, and adds it to App.js

---
+++ src/RoutingApp.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {
+    BrowserRouter as Router,
+    Link,
+    Route
+} from 'react-router-dom';
+
+function RoutingApp(){
+    return (
+        <Router>
+
+        </Router>
+    )
+}
+
+export default RoutingApp;
